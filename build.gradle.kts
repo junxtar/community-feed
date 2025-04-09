e2a37e402edaed18399c5f3266d36bcea20c0a6a
Junxtar <aa1870088@naver.com>
2025-04-10 07:54:41 +0900

    feat: thymeleaf template 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
 
+    // thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
     // mysql
     runtimeOnly("com.mysql:mysql-connector-j")
 
